1f9588efa61ee433fdc6215b3c09a13b2293846c
NatalliaMaskalenko <MoskNatulya@gmail.com>
2021-05-29 10:53:11 +0200

    Добавляет новый скрипт к скроллу

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,22 +1,18 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[mobile-menu-open]"),
-    closeModalBtn: document.querySelector("[mobile-menu-close]"),
-    modal: document.querySelector("[mobile-menu]"),
-    mobileNavList: document.querySelector('[data-menu-list]'),
-     };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-  refs.mobileNavList.addEventListener("click", toggleModal);
- 
-  
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-   
-})();
-
-
-
+;(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]")
+  const mobileMenuRef = document.querySelector("[data-menu]")
+  const mobileNavList = document.querySelector('[data-menu-list]');
+  menuBtnRef.addEventListener("click", () => {
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
+    menuBtnRef.classList.toggle("is-open")
+    menuBtnRef.setAttribute("aria-expanded", !expanded)
+    mobileMenuRef.classList.toggle("is-open")
+  })
+  mobileNavList.addEventListener("click", () => {
+    const expanded = mobileNavList.getAttribute("aria-expanded") === "true" || false
+    mobileMenuRef.classList.toggle('is-open');
+    mobileNavList.setAttribute("aria-expanded", !expanded)
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-active');
+  })
+})()
--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,35 @@
-  $(document).ready(function(){
+  // $(document).ready(function(){
    
-    $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-    $('.scrollup').fadeIn();
+  //   $(window).scroll(function(){
+  //   if ($(this).scrollTop() > 100) {
+  //   $('.scrollup').fadeIn();
+  //   } else {
+  //   $('.scrollup').fadeOut();
+  //   }
+  //   });
+     
+  //   $('.scrollup').click(function(){
+  //   $("html, body").animate({ scrollTop: 0 }, 600);
+  //   return false;
+  //   });
+     
+  //   });
+
+  mybutton = document.getElementById("myBtn");
+
+  // Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
     } else {
-    $('.scrollup').fadeOut();
+      mybutton.style.display = "none";
     }
-    });
-     
-    $('.scrollup').click(function(){
-    $("html, body").animate({ scrollTop: 0 }, 600);
-    return false;
-    });
-     
-    });
+  }
+  
+  // Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
+  function topFunction() {
+    document.body.scrollTop = 0; // Для Safari
+    document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+  }
